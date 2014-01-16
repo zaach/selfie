2bbd370aea97f67939841dfce1ff4aaf7647ec2b
Zachary Carter <zack.carter@gmail.com>
2014-01-15 22:21:34 -0800

    clean up http example

--- examples/http.js
+++ examples/http.js
@@ -3,20 +3,12 @@ var http = require('http');
 var url = require('url');
 var selfie = require('../');
 
-
 http.createServer(function(req, res){
-  console.log('again???', req.url);
-
   if (req.url === '/' && !req.headers.referer) {
-    console.log(req.headers);
     selfie()
       .on('capture', function(camera) {
-        console.log('pipe it!');
         res.writeHead(200, {'Content-Type': 'image/jpeg' });
         camera.stdout.pipe(res);
-        camera.stdout.on('exit', function() {
-          res.end();
-        });
       });
   }
 }).listen(8080);
